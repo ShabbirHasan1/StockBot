0a2719fdc3232821f5ba45664bc8406007bf7545
unknown <Sukrit>
2020-03-02 20:12:20 +0800

    try catch on holdings

--- holdings.py
+++ holdings.py
@@ -1,6 +1,13 @@
 import order
+import generate_token
+import kiteconnect
+
 def main():
-	print order.getHoldings()
+	try:
+		print order.getHoldings()
+	except kiteconnect.exceptions.TokenException as e:
+		generate_token.main()
+		
 
 if __name__ == "__main__":
     main()
