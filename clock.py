ca337b4745515caef7794c5001e829a3cb2e13f0
sukrit1990 <sukrit1990@yahoo.co.in>
2021-07-28 16:06:05 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -66,10 +66,10 @@ def scheduled_job4():
 	Median.main()
 	print 'This job is run every weekday at 11:20.'
 	
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=3, minute=39)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=8, minute=10)
 def scheduled_job2():
 	ScoreBuyStocks.main()
-	print 'This job is run every weekday at 11:39.'
+	print 'This job is run every weekday at 16:10.'
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=6, minute=00)
 # def scheduled_job():
